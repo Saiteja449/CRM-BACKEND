46987e34891eb3aa9fad87050bc50a02ddc24668
Saiteja449 <saiteja.infasta@gmail.com>
2026-06-29 17:11:06 +0530

    done live code added success messages 2

--- helpers/emailHelper.js
+++ helpers/emailHelper.js
@@ -5,7 +5,8 @@ export const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: process.env.SMTP_PORT,
-      secure: process.env.SMTP_PORT == 465, // true for 465, false for other ports
+      secure: process.env.SMTP_PORT == 465,
+      family: 4, // true for 465, false for other ports
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
